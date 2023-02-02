0205d12ca50c682de48d4b40d0032a275e8c400c
GopalAgarwal19 <56127731+GopalAgarwal19@users.noreply.github.com>
2023-02-02 17:29:59 +0800

    oop

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import random
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-import string
-app = Flask(__name__)
+import uuid
 
+app = Flask(__name__)
 
 diseases = {
     "A": {"COI": 75, "HC": 80, "Time": 2},
@@ -22,112 +22,85 @@ diseases = {
     "L": {"COI": 40, "HC": 25, "Time": 1},
 }
 
-cev_s1 = 0
-year_s1 = 0
-quarter_s1 = 0
-options_selected_s1  = 0
-alloted_diseases_s1  = 0
-checkups_s1  = 0
-cev_s2 = 0
-year_s2 = 0
-quarter_s2 = 0
-options_selected_s2  = 0
-alloted_diseases_s2  = 0
-checkups_s2  = 0
-deductible  = 0
-details  = 0
+userList = {}
 
-def set_values():
-    global cev_s1
-    global year_s1
-    global quarter_s1
-    global options_selected_s1 
-    global alloted_diseases_s1 
-    global checkups_s1 
-    global cev_s2
-    global year_s2
-    global quarter_s2
-    global options_selected_s2 
-    global alloted_diseases_s2 
-    global checkups_s2 
-    global deductible 
-    global details 
+class LocalVariables:
+     def __init__(self, cev_s1, year_s1, quarter_s1, options_selected_s1, alloted_diseases_s1, checkups_s1, cev_s2, year_s2, quarter_s2, options_selected_s2, alloted_diseases_s2, checkups_s2, deductible, details):
+        self.cev_s1 = cev_s1
+        self.year_s1 = year_s1
+        self.quarter_s1 = quarter_s1
+        self.options_selected_s1 = options_selected_s1
+        self.alloted_diseases_s1 = alloted_diseases_s1
+        self.checkups_s1 = checkups_s1
+        self.cev_s2 = cev_s2
+        self.year_s2 = year_s2
+        self.quarter_s2 = quarter_s2
+        self.options_selected_s2 = options_selected_s2
+        self.alloted_diseases_s2 = alloted_diseases_s2
+        self.checkups_s2 = checkups_s2
+        self.deductible = deductible
+        self.details = details
 
-    cev_s1 = 10000
-    year_s1 = 1
-    quarter_s1 = 1
-    options_selected_s1 = []
-    alloted_diseases_s1 = []
-    checkups_s1 = []
-
-    cev_s2 = 10000
-    year_s2 = 1
-    quarter_s2 = 1
-    options_selected_s2 = []
-    alloted_diseases_s2 = []
-    checkups_s2 = []
-    deductible = 75
-    details = {}
-    
+  
 @app.route("/", methods=["GET", "POST"])
 def home():
-    set_values()
-    global details
+    global userList
     if request.method == "POST":
-        details["age"] = request.form["age"]
-        details["gender"] = request.form["gender"]
-        details["year"] = request.form["year"]
-        details["course"] = request.form["course"]
-        print(details)
-        return redirect("/s1_ins")
+        currentUser = LocalVariables(cev_s1 = 10000,year_s1 = 1,quarter_s1 = 1,options_selected_s1 = [],alloted_diseases_s1 = [],checkups_s1 = [],cev_s2 = 10000,year_s2 = 1,quarter_s2 = 1,options_selected_s2 = [],alloted_diseases_s2 = [],checkups_s2 = [],deductible = 75,details = {}) 
+        myuuid = str(uuid.uuid4())
+        userList[myuuid] = currentUser
+        print(myuuid)
+        currentUser.details["age"] = request.form["age"]
+        currentUser.details["gender"] = request.form["gender"]
+        currentUser.details["year"] = request.form["year"]
+        currentUser.details["course"] = request.form["course"]
+        return redirect(url_for("s1_ins", myuuid = myuuid))
     return render_template("index.html")
 
 
 @app.route("/s1_ins", methods=["GET", "POST"])
 def s1_ins():
+    myuuid = request.args['myuuid']
+    print(myuuid)
     if request.method == "POST":
-        return redirect("/s1_game")
+        return redirect(url_for("s1_game", myuuid = myuuid))
     return render_template("s1_ins.html")
 
 
 @app.route("/s1_game", methods=["GET", "POST"])
 def s1_game():
-    global cev_s1
-    global year_s1
-    global quarter_s1
-    global options_selected_s1
-    global alloted_diseases_s1
-    global checkups_s1
-
+    global userList
+    myuuid = request.args['myuuid']
+    currentUser = userList[myuuid]
     random_disease = chr(random.randint(ord("A"), ord("L")))
-    alloted_diseases_s1.append(random_disease)
+    currentUser.alloted_diseases_s1.append(random_disease)
     if request.method == "POST":
         print(request.form["options"])
-        options_selected_s1.append(request.form["options"])
-        cev_s1 -= int(request.form["options"])
-        cev_s1 += 3000
+        currentUser.options_selected_s1.append(request.form["options"])
+        currentUser.cev_s1 -= int(request.form["options"])
+        currentUser.cev_s1 += 3000
 
-        if quarter_s1 == 2:
+        if currentUser.quarter_s1 == 2:
             print(request.form["checkup"])
-            cev_s1 -= int(request.form["checkup"])
-            checkups_s1.append(int(request.form["checkup"]))
+            currentUser.cev_s1 -= int(request.form["checkup"])
+            currentUser.checkups_s1.append(int(request.form["checkup"]))
 
-        if quarter_s1 == 4:
-            year_s1 += 1
-            quarter_s1 = 1
+        if currentUser.quarter_s1 == 4:
+            currentUser.year_s1 += 1
+            currentUser.quarter_s1 = 1
         else:
-            quarter_s1 += 1
+            currentUser.quarter_s1 += 1
 
-        if year_s1 == 4:
-            return redirect("/s2_ins")
+        if currentUser.year_s1 == 2:
+            return redirect(url_for("s2_ins", myuuid = myuuid))
 
-        return redirect("/s1_game")
+        return redirect(url_for("s1_game", myuuid = myuuid))
 
     return render_template(
         "s1_game.html",
-        cev=cev_s1,
-        year=year_s1,
-        quarter=quarter_s1,
+        cev=currentUser.cev_s1,
+        year=currentUser.year_s1,
+        quarter=currentUser.quarter_s1,
         random_disease=random_disease,
         coi=diseases[random_disease]["COI"],
         coh=diseases[random_disease]["HC"],
@@ -137,67 +110,67 @@ def s1_game():
 
 @app.route("/s2_ins", methods=["GET", "POST"])
 def s2_ins():
+    myuuid = request.args['myuuid']
     if request.method == "POST":
-        return redirect("/s2_game")
+        return redirect(url_for("s2_game", myuuid = myuuid))
     return render_template("s2_ins.html")
 
 
 @app.route("/s2_game", methods=["GET", "POST"])
 def s2_game():
-    global cev_s2
-    global year_s2
-    global quarter_s2
-    global options_selected_s2
-    global alloted_diseases_s2
-    global checkups_s2
-    global deductible
+    global userList
+    myuuid = request.args['myuuid']
+    currentUser = userList[myuuid]
+    
     random_disease = chr(random.randint(ord("A"), ord("L")))
-    alloted_diseases_s2.append(random_disease)
+    currentUser.alloted_diseases_s2.append(random_disease)
 
     if request.method == "POST":
         print(request.form["options"])
-        options_selected_s2.append(request.form["options"])
+        currentUser.options_selected_s2.append(request.form["options"])
         os = int(request.form["options"])
-        if quarter_s2 == 2:
+        if currentUser.quarter_s2 == 2:
             print(request.form["checkup"])
             os += int(request.form["checkup"])
-            checkups_s2.append(int(request.form["checkup"]))
-        if deductible >= os:
-            cev_s2 -= os
-            deductible -= os
+            currentUser.checkups_s2.append(int(request.form["checkup"]))
+        if currentUser.deductible >= os:
+            currentUser.cev_s2 -= os
+            currentUser.deductible -= os
         else:
-            cev_s2 -= deductible
-            cev_s2 -= 0.2 * (os - deductible)
-            deductible = 0
-        cev_s2 += 2500
+            currentUser.cev_s2 -= currentUser.deductible
+            currentUser.cev_s2 -= 0.2 * (os - currentUser.deductible)
+            currentUser.deductible = 0
+        currentUser.cev_s2 += 2500
 
-        if quarter_s2 == 4:
-            year_s2 += 1
-            quarter_s2 = 1
-            deductible = 75
+        if currentUser.quarter_s2 == 4:
+            currentUser.year_s2 += 1
+            currentUser.quarter_s2 = 1
+            currentUser.deductible = 75
         else:
-            quarter_s2 += 1
+            currentUser.quarter_s2 += 1
     
-        if year_s2 == 4:
-            return redirect("/thank")
-        return redirect("/s2_game")
+        if currentUser.year_s2 == 2:
+            return redirect(url_for("thank", myuuid = myuuid))
+        return redirect(url_for("s2_game", myuuid = myuuid))
 
     return render_template(
         "s2_game.html",
-        cev=cev_s2,
-        year=year_s2,
-        quarter=quarter_s2,
+        cev=currentUser.cev_s2,
+        year=currentUser.year_s2,
+        quarter=currentUser.quarter_s2,
         random_disease=random_disease,
         coi=diseases[random_disease]["COI"],
         coh=diseases[random_disease]["HC"],
         time=diseases[random_disease]["Time"],
-        deductible=deductible,
+        deductible=currentUser.deductible,
     )
 
 
 @app.route("/thank", methods=["GET", "POST"])
 def thank():
-    global details
+    global userList
+    myuuid = request.args['myuuid']
+    currentUser = userList[myuuid]
     try:
         app = firebase_admin.get_app()
     except ValueError as e:
@@ -206,64 +179,28 @@ def thank():
     # cred = credentials.Certificate('moral-hazard-game-firebase-adminsdk-1g1hk-0a4993c229.json')
     # firebase_admin.initialize_app(cred)
     db = firestore.client()
-    doc_ref = db.collection("tutorial").document(''.join(random.choices(string.ascii_letters, k=15)))
+    doc_ref = db.collection("tutorial").document(myuuid)
     doc_ref.set({
-        "Age": details["age"],
-                "Gender": details["gender"],
-                "Year": details["year"],
-                "Course": details["course"],
+        "Age": currentUser.details["age"],
+                "Gender": currentUser.details["gender"],
+                "Year": currentUser.details["year"],
+                "Course": currentUser.details["course"],
                 "S1 diseases": [
-                    alloted_diseases_s1[i]
-                    for i in range(len(alloted_diseases_s1))
+                    currentUser.alloted_diseases_s1[i]
+                    for i in range(len(currentUser.alloted_diseases_s1))
                     if i % 2 == 0
                 ],
-                "S1 options": options_selected_s1,
-                "S1 checkups": checkups_s1,
-                "S1 final CEV": cev_s1,
+                "S1 options": currentUser.options_selected_s1,
+                "S1 checkups": currentUser.checkups_s1,
+                "S1 final CEV": currentUser.cev_s1,
                 "S2 diseases": [
-                    alloted_diseases_s2[i]
-                    for i in range(len(alloted_diseases_s2))
+                    currentUser.alloted_diseases_s2[i]
+                    for i in range(len(currentUser.alloted_diseases_s2))
                     if i % 2 == 0
                 ],
-                "S2 options": options_selected_s2,
-                "S2 checkups": checkups_s2,
-                "S2 final CEV": cev_s2,
+                "S2 options": currentUser.options_selected_s2,
+                "S2 checkups": currentUser.checkups_s2,
+                "S2 final CEV": currentUser.cev_s2,
     })
-    # client = MongoClient(
-    #     "mongodb+srv://gopal:gopal%40123@cluster0.2hulrxt.mongodb.net/HDBLookUp?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE"
-    # )
-    # client = MongoClient(config('MONGODB_URI'))
-    # db = client["HE3604"]
-    # return render_template("thank.html")
-    # db["Tut 3"].insert_many(
-    #     [
-    #         {
-    #             "Age": details["age"],
-    #             "Gender": details["gender"],
-    #             "Year": details["year"],
-    #             "Course": details["course"],
-    #             "S1 diseases": [
-    #                 alloted_diseases_s1[i]
-    #                 for i in range(len(alloted_diseases_s1))
-    #                 if i % 2 == 0
-    #             ],
-    #             "S1 options": options_selected_s1,
-    #             "S1 checkups": checkups_s1,
-    #             "S1 final CEV": cev_s1,
-    #             "S2 diseases": [
-    #                 alloted_diseases_s2[i]
-    #                 for i in range(len(alloted_diseases_s2))
-    #                 if i % 2 == 0
-    #             ],
-    #             "S2 options": options_selected_s2,
-    #             "S2 checkups": checkups_s2,
-    #             "S2 final CEV": cev_s2,
-    #         }
-    #     ]
-    # )
-
+    
     return render_template("thank.html")
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
